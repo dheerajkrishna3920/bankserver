e3b262130064f0fd534f5b1196b3383ca9bb1e79
Dheeraj Krishna K <dheerajkrishna3920@gmail.com>
2023-06-13 23:16:10 +0530

    final

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 //import express
 
-const express=require("express")
+const express = require("express")
+
+
+//import jwt
+const jwt = require('jsonwebtoken')
+
+
+//app(server) creation
+
+const app = express()
 
 
 //import logic file
 
-const logic=require('./service/logic')
+const logic = require('./service/logic')
 
-//app(server) creation
-
-const app=express()
 
 
 // integrate frontend with server
 
-const cors=require('cors')
-app.use(cors({origin:'http://localhost:4200/'}))
+const cors = require('cors')
+app.use(cors({ origin: 'http://localhost:4200' }))
 
 
 
@@ -25,32 +31,55 @@ app.use(express.json())
 
 
 
+//middleware creation
+
+const jwtMiddleware = (req, res, next) => {
+    console.log("........middleware.....");
+
+
+    try {//acess token from request header
+
+        const token = req.headers['acess_token']
+
+        //verify
+        jwt.verify(token, "secretkey123")
+
+        next()
+    }
+    catch {
+        res.status(404).json({
+            statusCode: 404,
+            status: false,
+            message: "Unauthorized user"
+        })
+    }
+
+}
+
 
 
 //app.get('/getdata',(req,res)=>{
-  //  console.log(req.body.acno);
-    //res.send("get method...")
+//  console.log(req.body.acno);
+//res.send("get method...")
 //})
 
 //to convert all outcoming js data in to json and send response
 
-app.get('/getdata',(req,res)=>{
-    console.log(req.body.acno);
+app.get('/getdata', (req, res) => {
+    //console.log(req.body.acno);
     res.json(req.body.acno)
 })
 
-//port set
 
-app.listen(3000,()=>{
-    console.log("server started at port 3000");
-})
+
+
 
 
 //request register
 
-app.post('/register',(req,res)=>{
-    
-    logic.register(req.body.acno,req.body.uname,req.body.psw).then(result=>{
+app.post('/register', (req, res) => {
+
+    logic.register(req.body.acno, req.body.uname, req.body.psw).then(result => {
         res.status(result.statusCode).json(result)
     })
 })
@@ -58,9 +87,63 @@ app.post('/register',(req,res)=>{
 
 //login request
 
-app.post('/login',(req,res)=>{
+app.post('/login', (req, res) => {
 
-    logic.login(req.body.acno,req.body.psw).then(result=>{
+    logic.login(req.body.acno, req.body.psw).then(result => {
         res.status(result.statusCode).json(result)
     })
+})
+
+
+//balance
+app.get('/balance/:acno', jwtMiddleware, (req, res) => {
+    logic.getBalance(req.params.acno).then(result => {
+        res.status(result.statusCode).json(result)
+    })
+
+})
+
+//singleuser
+app.get('/getUser/:acno', jwtMiddleware, (req, res) => {
+    logic.getUser(req.params.acno).then(result => {
+        res.status(result.statusCode).json(result)
+    })
+
+})
+
+//fund transfer
+
+app.post('/transfer', jwtMiddleware, (req, res) => {
+    logic.fundTransfer(
+        req.body.toAcno,
+        req.body.fromAcno,
+        req.body.amount,
+        req.body.psw,
+        req.body.date
+    )
+        .then(result => {
+            res.status(result.statusCode).json(result)
+        })
+})
+
+//transaction history
+
+app.get('/transaction/:acno', jwtMiddleware, (req, res) => {
+    logic.getTransaction(req.params.acno).then(result => {
+        res.status(result.statusCode).json(result)
+    })
+})
+
+
+app.delete('/deleteAc/:acno', jwtMiddleware, (req, res) => {
+    logic.deleteAc(req.params.acno).then(result => {
+        res.status(result.statusCode).json(result)
+    })
+})
+
+
+
+//port set
+app.listen(3000, () => {
+    console.log("server started at port 3000");
 })
--- service/logic.js
+++ service/logic.js
@@ -2,12 +2,16 @@
 
 const db = require('./db')
 
+//import jwt
+const jwt = require('jsonwebtoken')
+
 
 //create a fuunction for register logic
 
 register = (acno, uname, psw) => {
     return db.User.findOne({ acno: acno }).then(User => {
         if (User) {
+
             return {
                 message: "User already exists",
                 status: false,
@@ -46,22 +50,26 @@ register = (acno, uname, psw) => {
 
 
 
-login=(acno,psw)=>{
+login = (acno, psw) => {
 
-    return db.User.findOne({acno:acno,pasw:psw}).then(user=>{
-        if(user){
-            return{
+    return db.User.findOne({ acno: acno, pasw: psw }).then(user => {
+        if (user) {
+
+            const token = jwt.sign({ currentAcno: acno }, "secretkey123")
+
+            return {
                 message: "login successfull",
                 status: true,
                 statusCode: 200,
-                currentUser:user.uname,
-                currentAcno:user.acno
+                currentUser: user.uname,
+                currentAcno: user.acno,
+                token: token
             }
         }
-        else{
+        else {
 
-            return{
-                message: "Incorrect Password",
+            return {
+                message: "Incorrect Accountnumber or Password",
                 status: false,
                 statusCode: 404
             }
@@ -69,9 +77,176 @@ login=(acno,psw)=>{
     })
 }
 
-module.exports={
-    register,login
+
+
+
+getBalance = acno => {
+    return db.User.findOne({ acno: acno }).then(user => {
+        if (user) {
+            return {
+                message: user.balance,
+                status: true,
+                statusCode: 200,
+            }
+        }
+        else {
+            return {
+                message: "Incorrect Account number",
+                status: false,
+                statusCode: 404
+            }
+        }
+    })
 }
+
+
+
+
+
+getUser = acno => {
+    return db.User.findOne({ acno: acno }).then(user => {
+        if (user) {
+            return {
+                message: user,
+                status: true,
+                statusCode: 200,
+            }
+        }
+        else {
+            return {
+                message: "Incorrect Account number",
+                status: false,
+                statusCode: 404
+            }
+        }
+    })
+}
+
+
+
+fundTransfer = (toAcno, fromAcno, amount, psw, date) => {
+    let amnt = parseInt(amount)
+    return db.User.findOne({ acno: fromAcno, pasw: psw }).then(result => {
+        if (result) {
+            return db.User.findOne({ acno: toAcno }).then(data => {
+                if (data) {
+                    if (amnt > result.balance) {
+                        return {
+                            message: "Insufficeint balance",
+                            status: false,
+                            statusCode: 404
+                        }
+                    }
+                    else {
+                        result.balance = result.balance - amnt
+                        result.transactions.push(
+                            {
+                                type: "DEBIT",
+                                amount: amnt,
+                                date: date
+                            }
+                        )
+                        result.save()
+
+                        data.balance = data.balance + amnt
+                        data.transactions.push(
+                            {
+                                type: "CREDIT",
+                                amount: amnt,
+                                date: date
+                            }
+                        )
+                        data.save()
+
+
+                        return {
+                            message: "transaction successfull",
+                            status: true,
+                            statusCode: 200,
+                            newBalance: result.balance
+                        }
+                    }
+                }
+                else {
+                    return {
+                        message: "Invalid credit credential",
+                        status: false,
+                        statusCode: 404
+                    }
+                }
+            })
+        }
+        else {
+            return {
+                message: "Invalid debit credential",
+                status: false,
+                statusCode: 404
+            }
+        }
+    })
+}
+
+
+
+
+
+getTransaction = (acno) => {
+    return db.User.findOne({ acno }).then(user => {
+        if (user) {
+            return {
+                message: user.transactions,
+                status: true,
+                statusCode: 200
+
+            }
+
+        }
+        else {
+            return {
+                message: "Invalid user",
+                status: false,
+                statusCode: 404
+            }
+        }
+    })
+}
+
+
+
+
+
+
+
+
+deleteAc = (acno) => {
+    return db.User.deleteOne({ acno }).then(deleteCount => {
+        if (deleteCount) {
+            return {
+                message: "user deleted",
+                status: true,
+                statusCode: 200
+
+            }
+        }
+        else {
+            return {
+                message: "Invalid user",
+                status: false,
+                statusCode: 404
+            }
+        }
+    })
+}
+
+
+
+
+
+module.exports = {
+    register, login, getBalance, getUser, fundTransfer,
+    getTransaction, deleteAc
+}
+
 
 
 
